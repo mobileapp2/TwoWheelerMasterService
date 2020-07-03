7ae76614872db44a2ed066bc8aa7c7e51b65ace1
dhanu55patil <dhanu204349magdum@gmail.com>
2020-07-03 15:46:08 +0530

    pojo without mapping

--- src/main/java/com/webo/app/twowheelerloan/masterservice/main/TwoWheelerLoanApplication.java
+++ src/main/java/com/webo/app/twowheelerloan/masterservice/main/TwoWheelerLoanApplication.java
@@ -2,13 +2,14 @@ package com.webo.app.twowheelerloan.masterservice.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
-@ComponentScan("com.webo.app.twowheelerloan.masterservice")
+@EnableJpaRepositories
+
+
+
 public class TwoWheelerLoanApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/webo/app/twowheelerloan/masterservice/main/model/Tenure.java
@@ -0,0 +1,40 @@
+package com.webo.app.twowheelerloan.masterservice.main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tenure {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer tenureId;
+	private int noOfYears;
+	private int status;
+	public Integer getTenureId() {
+		return tenureId;
+	}
+	public void setTenureId(Integer tenureId) {
+		this.tenureId = tenureId;
+	}
+	public int getNoOfYears() {
+		return noOfYears;
+	}
+	public void setNoOfYears(int noOfYears) {
+		this.noOfYears = noOfYears;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "Tenure [tenureId=" + tenureId + ", noOfYears=" + noOfYears + ", status=" + status + "]";
+	}
+	
+	
+
+}
